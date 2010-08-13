56fa2f0ceddda607574bffef8785688879b3ed0c
deeplovepan <deeplovepan@gmail.com>
2010-08-13 23:39:09 +0800

    commit

---
+++ TwitterTestViewController.m
@@ -0,0 +1,100 @@
+//
+//  TwitterTestViewController.m
+//  MyLib
+//
+//  Created by deeplove on 2010/8/13.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "TwitterTestViewController.h"
+
+
+@implementation TwitterTestViewController
+
+/*
+ // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	if (twitterEngine)
+	{
+		return;
+	}
+	twitterEngine = [[SA_OAuthTwitterEngine alloc] initOAuthWithDelegate: self];
+	[twitterEngine setConsumerKey:kOAuthConsumerKey secret:kOAuthConsumerSecret];
+	
+	UIViewController *controller = [SA_OAuthTwitterController 
+									controllerToEnterCredentialsWithTwitterEngine:twitterEngine delegate: self];
+	
+	if (controller) 
+	{
+		[self presentModalViewController: controller animated: YES];
+	}
+}
+
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark TwitterEngineDelegate
+- (void) requestSucceeded: (NSString *) requestIdentifier {
+	NSLog(@"Request %@ succeeded", requestIdentifier);
+}
+
+- (void) requestFailed: (NSString *) requestIdentifier withError: (NSError *) error {
+	NSLog(@"Request %@ failed with error: %@", requestIdentifier, error);
+}
+
+
+#pragma mark -
+#pragma mark SA_OAuthTwitterControllerDelegate
+- (void) OAuthTwitterController: (SA_OAuthTwitterController *) controller authenticatedWithUsername: (NSString *) username {
+	NSLog(@"Authenicated for %@", username);
+	// can not send the same tweet twice
+	[twitterEngine sendUpdate:@"test2"];
+
+}
+
+- (void) OAuthTwitterControllerFailed: (SA_OAuthTwitterController *) controller {
+	NSLog(@"Authentication Failed!");
+}
+
+- (void) OAuthTwitterControllerCanceled: (SA_OAuthTwitterController *) controller {
+	NSLog(@"Authentication Canceled.");
+}
+
+
+@end
